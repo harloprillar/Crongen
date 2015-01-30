c2acfb3f02cd90f2e72af98d31297c7cedfc985e
admin <admin@vtochku.net>
2015-01-30 18:49:19 +0200

    fix

--- crongen.py
+++ crongen.py
@@ -10,7 +10,7 @@ from optparse import OptionParser
 
 pars = OptionParser("usage: %prog -e INTERVAL [options]", add_help_option=False)
 pars.add_option("-e", "--every", type="int", dest="interval", help="interval in minutes to run command")
-pars.add_option("-c", "--command", type="string", dest="command", default="command", help="command to run")
+pars.add_option("-c", "--command", type="string", dest="command", help="command to run")
 pars.add_option("-h", "--hour", type="int", dest="start_hour", default=0, help="start hour(default 0)")
 pars.add_option("-m", "--minute", type="int", dest="start_min", default=0, help="start minute(default 0)")
 (options, args) = pars.parse_args()
@@ -20,8 +20,9 @@ if not options.interval:
     raise SystemExit(0)
 
 def add_min(date, minute):
-    ddate = date + timedelta(minutes=minute)
-    day, hour, minute = (int(x) for x in re.findall('^[0-9]{4}-[0-9]{2}-([0-9]{2}) ([0-9]{2}):([0-9]{2})', str(ddate))[0])
+    date = date + timedelta(minutes=minute)
+    regex = "^[0-9]{4}-[0-9]{2}-([0-9]{2}) ([0-9]{2}):([0-9]{2})"
+    day, hour, minute = (int(x) for x in re.findall(regex, str(date))[0])
     return day, hour, minute
 
 def normalize(arr):
@@ -39,8 +40,13 @@ def normalize(arr):
             if l == 1:
                 return hours
             return hours+"/"+str(l)
-            break
     return ",".join(str(x) for x in arr)
+
+def printout(m, h):
+    cmd = ""
+    if options.command:
+        cmd = options.command
+    print(str(m)+" "+h+" * * * "+cmd)
 
 date = datetime(1, 1, 1, options.start_hour, options.start_min)
 new = add_min(date, 0)
@@ -72,9 +78,9 @@ for i in range(len(arr_h_norm)-1):
         arr_min = []
         for i in dict_min:
             arr_min.append(i)
-        print(normalize(sorted(arr_min)) + " " +arr_h_norm[0]+ " * * * " + options.command)
+        printout(normalize(sorted(arr_min)), arr_h_norm[0])
         sys.exit(0)
 
 for i in dict_min:
-    print(str(i) + " " +normalize(dict_min[i])+ " * * * " + options.command)
+    printout(i, normalize(dict_min[i]))
 
